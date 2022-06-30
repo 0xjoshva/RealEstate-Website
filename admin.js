2d330dcb5f2e84460403633a651bd7497061d0fe
0xjoshva <joshuasteed123@gmail.com>
2022-06-30 19:42:47 +0200

    changed array and nput placeholders

--- admin.js
+++ admin.js
@@ -161,6 +161,38 @@ let arrProperties = JSON.parse(localStorage.getItem("arrProperties"))
         price: 2450,
         category: "APARTMENT",
       },
+      {
+        imageURL:
+          "https://github.com/0xjoshva/RealEstate-Website/blob/main/assets/black-12.jpg?raw=true",
+        title: "Upscale Office",
+        address: "9213 Long St, Cape Town, WC 9283, ZA ",
+
+        description:
+          "Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quod mazim placerat facer possim assum. Typi non habent claritatem insitam; est usus legentis in iis qui facit eorum claritatem. Investigationes demonstraverunt lectores legere me lius quod ii legunt saepius. Claritas est etiam processus dynamicus, qui sequitur mutationem consuetudium lectorum. Mirum est notare quam littera gothica, quam nunc putamus parum claram, anteposuerit litterarum formas humanitatis per seacula quarta decima et quinta decima. Eodem modo typi, qui nunc nobis videntur parum clari, fiant sollemnes in futurum.",
+
+        bedrooms: 6,
+        bathrooms: 3,
+        garages: 3,
+        size: 1509,
+        price: 9950,
+        category: "OFFICE",
+      },
+      {
+        imageURL:
+          "https://github.com/0xjoshva/RealEstate-Website/blob/main/assets/black-11.jpg?raw=true",
+        title: "Breathtaking Modern Office",
+        address: "81 Adderley St, Cape Town, WC 1343, 90034 ",
+
+        description:
+          "Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quod mazim placerat facer possim assum. Typi non habent claritatem insitam; est usus legentis in iis qui facit eorum claritatem. Investigationes demonstraverunt lectores legere me lius quod ii legunt saepius. Claritas est etiam processus dynamicus, qui sequitur mutationem consuetudium lectorum. Mirum est notare quam littera gothica, quam nunc putamus parum claram, anteposuerit litterarum formas humanitatis per seacula quarta decima et quinta decima. Eodem modo typi, qui nunc nobis videntur parum clari, fiant sollemnes in futurum.",
+
+        bedrooms: 6,
+        bathrooms: 4,
+        garages: 3,
+        size: 1509,
+        price: 6995,
+        category: "OFFICE",
+      },
     ];
 
 //change badge number when items added or removed
